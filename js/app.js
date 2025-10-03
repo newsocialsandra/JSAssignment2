4373de88a3309bb54a3f6dae1be69427d870e612
newsocialsandra <sandra.e.b.svensson@gmail.com>
2025-10-03 21:26:40 +0200

    Added function to get random monster

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ let playerName = "Penny";
 let playerAge = 18;
 let rightHand = "sword";
 let leftHand = null;
-let rollD20 = Math.floor(Math.random() * 20) + 1
+let rollD20 = Math.floor(Math.random() * 20) + 1;
+let rollD6 = Math.floor(Math.random() * 6) + 1;
+let monsters = ["spider", "kobold", "cultist", "swarm of bats", "minotaur skeleton", "chimera"];
 
 function welcomePlayer(name) {
   return "Welcome, " + name;
@@ -17,17 +19,17 @@ function checkEligibility(age) {
   }
 }
 
-function startArea(diceroll) {
+function startArea(roll) {
 
-  if(diceroll < 8) {
-    return "You rolled a " + diceroll + ". You start in a small village";
+  if(roll < 8) {
+    return "You rolled a " + roll + ". You start in a small village";
   }
-  else if(diceroll < 15) {
-    return "You rolled a " + diceroll + ". You start in a lush forest";
+  else if(roll < 15) {
+    return "You rolled a " + roll + ". You start in a lush forest";
   }
 
   else {
-    return "You rolled a " + diceroll + ". You start in a dark dungeon";
+    return "You rolled a " + roll + ". You start in a dark dungeon";
   }
 }
 
@@ -42,15 +44,21 @@ function chooseEquipment(rightHand,leftHand) {
     return "You only have a " + leftHand;
   }
   else {
-    "You are unarmed! Be careful!";
+    return "You are unarmed! Be careful!";
   }
 }
 
-function newGame(name, age, right, left) {
+function monsterAppears(roll) {
+  let monster = monsters[roll -1];
+  return "A " + monster + " appears. What do you do?"
+}
+
+function newGame() {
   console.log(welcomePlayer(playerName));
-  console.log(checkEligibility("playerAge"));
+  console.log(checkEligibility(playerAge));
   console.log(startArea(rollD20));
   console.log(chooseEquipment(rightHand, leftHand));
+  console.log(monsterAppears(rollD6));
 }
 
 newGame();
